81866dd2d7c28fbfc1e60f39ecc25e77e4738ace
overpas <pckeycalculator@gmail.com>
2023-06-05 21:10:53 +0400

    Use java toolchain

--- depstoml/build.gradle.kts
+++ depstoml/build.gradle.kts
@@ -37,6 +37,12 @@ dependencies {
     testImplementation(libs.kotlin.test)
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     kotlinOptions {
         jvmTarget = "17"
